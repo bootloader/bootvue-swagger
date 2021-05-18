180e9b66d937b8a482537f649b527e6456438c01
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-05-18 19:19:54 +0530

    added build script

--- build.sh
+++ build.sh
@@ -2,4 +2,14 @@
 
 NEWEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 
-echo $NEWEST_TAG
+echo "build@latest"
+
+npm run build
+mkdir -p build/app@latest
+mv dist build/app@latest/dist
+
+echo "build@${NEWEST_TAG}"
+git checkout "${NEWEST_TAG}"
+npm run build
+mkdir -p "build/app@${NEWEST_TAG}"
+mv dist "build/app@${NEWEST_TAG}/dist"
