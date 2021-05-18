5af3b868a94b02ba19c0eedc3e2b53978ca65a19
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-05-18 19:45:31 +0530

    updated build script

--- build.sh
+++ build.sh
@@ -2,14 +2,19 @@
 
 NEWEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 
+#Make latest Build from prodution branch
 echo "build@latest"
-
+git co production
 npm run build
 mkdir -p build/app@latest
 mv dist build/app@latest/dist
 
+#Make latest Build from Latest Tag
 echo "build@${NEWEST_TAG}"
 git checkout "${NEWEST_TAG}"
 npm run build
 mkdir -p "build/app@${NEWEST_TAG}"
 mv dist "build/app@${NEWEST_TAG}/dist"
+
+#Rest Everything Back to Production Branch
+git co production
