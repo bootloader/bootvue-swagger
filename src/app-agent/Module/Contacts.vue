f5fb17b37da00a1e2f7e314fcbd9db820cad319d
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-11-05 03:27:49 +0530

    Addded new SEARCH Session Flag

--- src/app-agent/Module/Contacts.vue
+++ src/app-agent/Module/Contacts.vue
@@ -14,7 +14,7 @@
                     <span v-if="!!search.text" class="input-group-text search_btn" @click="search.text=''" ><i class="fa fa-close"></i></span>
                     <span v-if="!search.text" class="input-group-text search_btn" ><i class="fa fa-search"></i></span>
                 </div>
-                <div class="input-group-append" v-if="MyConst.config.AGENT_CHAT_INIT">
+                <div class="input-group-append" v-if="MyConst.config.AGENT_CHAT_SEARCH_SESSION">
                     <a class="input-group-text menu_btn new-chat fa fa-edit" v-b-toggle 
                     @click="MyFlags.agent.showContactSearch=!MyFlags.agent.showContactSearch"
                      @click.prevent>
