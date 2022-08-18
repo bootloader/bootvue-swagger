0c13daaf926d637fc24bad73852059d0f2cd951f
lalit.tanwar <lalit.tanwar07@gmail.com>
2022-08-18 10:38:47 +0530

    code clean up

--- src/app-admin/Modules/SetUp/SetUpConfiguration.vue
+++ src/app-admin/Modules/SetUp/SetUpConfiguration.vue
@@ -44,23 +44,12 @@
 </template>
 
 <script>
-
-    import PageTitle from "../../Components/PageTitle.vue";
-    import { MyFlags,MyDict,MyConst } from '../../../services/global';
     import SetupConfigurationKey from './SetupConfigurationKey.vue';
-
-    function newItem() {
-      return {
-              "config" : {},
-              "meta": {}
-            };
-    }
     export default {
         components: {
-            PageTitle,SetupConfigurationKey
+            SetupConfigurationKey
         },
         data: () => ({
-            MyFlags : MyFlags, MyDict : MyDict,MyConst : MyConst,
             heading: 'Configuration Setup',
             subheading: 'Control Agent Panel setting',
             icon: 'pe-7s-users icon-gradient bg-happy-itmeo fa fa-tools',
@@ -80,19 +69,8 @@
               groupBy : 'meta.group',
               busy : false
             },
-            oneItem : newItem(),
-            modelName :  "MODAL_ADD_USERS",
         }),
         computed : {
-            items : function (argument) {
-              return this.$store.getters.StateAgents
-            },
-            teams : function (argument) {
-              return this.$store.getters.StateTeams;
-            },
-            isChanged :  function (argument) {
-              return this.oldHash !== JSON.stringify(this.oneItem);
-            }
         },
         created : function (argument) {
           this.loadItems();
@@ -112,52 +90,8 @@
             } finally {
               this.table.busy=false
             }
-          },
-          async saveItem () {
-              await this.$store.dispatch('SetConfigs', {
-                key : this.oneItem.meta.key,value : this.oneItem.config.value,
-              });
-              this.oneItem = newItem();
-              this.onAction({name : "CANCEL"});
-              this.loadItems();
-          },
-          async cancelItem(item) {
-             this.oneItem = newItem();
-             this.onAction({name : "CANCEL"});
-          }, 
-          async editItem(item) {
-              this.oneItem = newItem();
-              for(var i in item){
-                this.oneItem[i] = JSON.parse(JSON.stringify(item[i]));
-              }
-              this.onAction({name : "EDIT_ITEM"});
-             //await this.$store.dispatch('DeleteQuickReps', item);
-          },
-          onAction : function (argument) {
-            switch(argument.name){
-              case "ADD_ITEM" :
-                this.oldHash = JSON.stringify(this.oneItem);
-                this.$bvModal.show(this.modelName)
-                console.log("ADD_ITEM",argument);
-                break;
-              case "EDIT_ITEM" :
-                this.oldHash = JSON.stringify(this.oneItem);
-                this.$bvModal.show(this.modelName)
-                console.log("ADD_ITEM",argument);
-                break;
-              case "CANCEL" :
-                this.$bvModal.hide(this.modelName)
-                break;
-              default:
-                console.log("NoMapping",argument) 
-            }
-          },
-
-
-
+          }
         }
-
-
     }
 </script>
 <style type="text/css">
