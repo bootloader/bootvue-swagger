4248babe2a719331aed3a1587246d7c58bb8348c
lalit.tanwar <lalit.tanwar07@gmail.com>
2023-04-12 00:30:14 +0530

    git config core.ignorecase false

---
+++ src/app-front/Index/IndexNavbar.vue
@@ -0,0 +1,167 @@
+<template>
+  <nav
+    class="top-0 z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+    :class="[position,'bg-'+bgColor,{
+      'shadow' : shadow
+    }]"
+  >
+    <div
+      class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+    >
+      <div
+        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+      >
+        <router-link to="/">
+          <a
+            class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+            :class="['text-'+textColor]"
+            href="#pablo"
+          >
+            {{$config.PROP_SERVICE_NAME}}
+          </a>
+        </router-link>
+        <button
+          class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          type="button"
+          v-on:click="setNavbarOpen"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
+      <div
+        class="lg:flex flex-grow items-center"
+        :class="[navbarOpen ? 'block' : 'hidden d-none']"
+        id="example-navbar-warning"
+      >
+        <ul class="flex flex-col lg:flex-row list-none mr-auto">
+          <li class="flex items-center">
+            <a
+              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+              :href="$config.PROP_SERVICE_DOCS_API_LINK"
+            >
+              <i
+                class="text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2"
+              />
+              Docs
+            </a>
+          </li>
+        </ul>
+        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+          <li class="flex items-center hidden" hidden>
+            <index-dropdown />
+          </li>
+          <li class="flex items-center" v-if="$config.PROP_SOCIAL_FACEBOOK">
+            <a
+              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+              :href="'https://facebook.com/' + $config.PROP_SOCIAL_FACEBOOK"
+              target="_blank"
+            >
+              <i class="text-blueGray-400 fab fa-facebook text-lg leading-lg" />
+              <span class="lg:hidden inline-block ml-2">Facebook</span>
+            </a>
+          </li>
+
+          <li class="flex items-center" v-if="$config.PROP_SOCIAL_TWITTER">
+            <a
+              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+              :href="'https://twitter.com/' + $config.PROP_SOCIAL_TWITTER"
+              target="_blank"
+            >
+              <i class="text-blueGray-400 fab fa-twitter text-lg leading-lg" />
+              <span class="lg:hidden inline-block ml-2">Tweet</span>
+            </a>
+          </li>
+
+          <li class="flex items-center" v-if="$config.PROP_SOCIAL_GITHUB">
+            <a
+              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+              :href="'https://github.com/' + $config.PROP_SOCIAL_GITHUB"
+              target="_blank"
+            >
+              <i class="text-blueGray-400 fab fa-github text-lg leading-lg" />
+              <span class="lg:hidden inline-block ml-2">Github</span>
+            </a>
+          </li>
+
+
+          <li class="flex items-center" v-if="$config.PROP_SOCIAL_INSTAGRAM">
+            <a
+              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+              :href="'https://instagram.com/' + $config.PROP_SOCIAL_INSTAGRAM"
+              target="_blank"
+            >
+              <i class="text-blueGray-400 fab fa-instagram text-lg leading-lg" />
+              <span class="lg:hidden inline-block ml-2">Instagram</span>
+            </a>
+          </li>
+
+          <li class="flex items-center">
+            <LoginDropdown
+              btnClass="bg-emerald-500 text-white active:bg-emerald-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150">
+            </LoginDropdown>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import IndexDropdown from "./IndexDropdown.vue";
+import LoginDropdown from "./LoginDropdown.vue";
+
+import jslocator from "@/services/jslocator";
+
+// jslocator.config({
+//   ipgeolocation : {
+//     apiKey : 'bbeeffef0d324caeac1fe3efa8565f10'
+//   }
+// });
+
+export default {
+  props : {
+    position : {
+      type: String,
+      default: 'fixed',
+    },
+    bgColor : {
+      type: String,
+      default: 'transparent',
+    },
+    shadow : {
+      type: Boolean,
+      default: false,
+    },
+    textColor : {
+      type: String,
+      default: 'blueGray-700',
+    },
+    textColor : {
+      type: String,
+      default: 'blueGray-700',
+    }
+  },
+  data() {
+    return {
+      navbarOpen: false,
+      country : null,
+    };
+  },
+  methods: {
+    setNavbarOpen: function () {
+      this.navbarOpen = !this.navbarOpen;
+    },
+    async updateCountry(){
+      let loc = await jslocator.get();
+      console.log("updateCountry",loc)
+      this.country = loc.country_name;
+    }
+  },
+  mounted(){
+    this.updateCountry();
+  },
+  components: {
+    IndexDropdown,LoginDropdown
+  },
+};
+</script>
