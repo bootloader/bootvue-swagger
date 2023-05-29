2374532566255d05899b400cf31cacb75e4693fe
amittnt <84276312+amittnt@users.noreply.github.com>
2023-05-29 18:32:07 +0530

    14

--- src/app-linq/Index/MyProfile.vue
+++ src/app-linq/Index/MyProfile.vue
@@ -18,7 +18,7 @@
         </div>
         <div class="section-wrapper1">
           <div class="social-tile-container ">
-            <social-tile class="w-full lg:w-4/12"
+            <social-tile class="w-full lg:w-4/12" style="text-align: right;"
               :title="meta.profile.name"  :subtitle="meta.profile.jobTitle" provider="google"> 
               <template #thumb>
                 <social-icon href="/linq/auth/logout" icon="fa fa-power-off" variant="evening"/>
